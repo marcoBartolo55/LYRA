841ba007f0b739a75ffb2c4c0a634e6ad4f3ea39
Marco <117793899+marcoBartolo55@users.noreply.github.com>
2023-05-11 20:18:39 -0600

    Prueba

--- LYRA/src/index.js
+++ LYRA/src/index.js
@@ -3,3 +3,5 @@ const app = require('./config/server');
 app.listen(app.get('port'),()=>{
     console.log('Servidor iniciado en el puerto: ', app.get('port'))
 });
+
+//Pero si vine cabronas, me lleva la mierda abuela. 🐸🐸🐸🐸🐸🐸
