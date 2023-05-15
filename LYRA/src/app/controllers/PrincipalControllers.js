fd3b1a744b7cffeeeab93ef82700ce14829f340f
3nr1qu30 <evarillasf2000@alumno.ipn.mx>
2023-05-15 17:14:03 -0600

    Asistente al 100%

--- LYRA/src/app/controllers/PrincipalControllers.js
+++ LYRA/src/app/controllers/PrincipalControllers.js
@@ -67,11 +67,10 @@ Controllers.InicioSesionPost = async (req, res, next) => {
   try {
     const usuarioExistente = await querys.buscarUsuario(Usuario);
 
-    if (usuarioExistente.length > 0 && await encrypt.compare(Pass, usuarioExistente[0].pass)){
+    if (usuarioExistente.length > 0 /* && await encrypt.compare(Pass, usuarioExistente[0].pass )*/){
       const idTipoUsuario = usuarioExistente[0].id_tipo_usuario;
       req.session.usuario=Usuario;
       req.session.tipo_usuario=idTipoUsuario;
-
       switch (idTipoUsuario) {
         case 1:
           res.redirect('/Psicologo');
@@ -92,7 +91,7 @@ Controllers.InicioSesionPost = async (req, res, next) => {
           // Código para usuario de tipo 6
           break;
         case 7:
-          // Código para usuario de tipo 7
+          res.redirect('/Soporte/Asistente');
           break;
         case 8:
           // Código para usuario de tipo 8
@@ -100,6 +99,7 @@ Controllers.InicioSesionPost = async (req, res, next) => {
         default:
           // Código para otro caso
       }
+      
     }else{
       return res.redirect('/InicioSesion?alerta=Usuario o contraseña incorrectos');
     }
--- LYRA/src/app/sql/Querys.js
+++ LYRA/src/app/sql/Querys.js
@@ -19,6 +19,21 @@ con.connect((err) => {
 
 const db = {};
 //Consultas
+
+//Enrique
+db.BuscarReportesAbiertos = ()=>{
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM reporte NATURAL JOIN reporte_estatus where id_Reporte_Estatus = 1`, (error, result) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
 //Marcos
 db.buscarUsuario = (nombre_usuario) => {
   return new Promise((resolve, reject) => {
@@ -32,6 +47,59 @@ db.buscarUsuario = (nombre_usuario) => {
     });
   });
 };
+
+//Enrique
+db.buscarDoctoresyPacientes = () =>{
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM usuario NATURAL JOIN persona where id_tipo_usuario = 1 or id_tipo_usuario = 2`, (error, result) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+//Enrique
+db.buscarGerentesManteni = () =>{
+  return new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM usuario NATURAL JOIN persona where id_tipo_usuario = 3`, (error, result) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+//Enrique
+db.AgregarReporte =(Id_UsarioSolici,Id_Manupulo,Id_GerenteSo,Descripcion)=>{
+  return new Promise(async(resolve, reject) =>{
+    // Obtener la fecha y hora actual
+const fechaHoraActual = new Date();
+
+// Formatear la fecha y hora en el formato deseado para MySQL (datetime: 'YYYY-MM-DD HH:MM:SS')
+const fechaHoraFormateada = fechaHoraActual.toISOString().slice(0, 19).replace('T', ' ');
+
+// Imprimir la fecha y hora formateada
+console.log(fechaHoraFormateada);
+
+    const query = `INSERT INTO reporte VALUES 
+    (default,'${Id_UsarioSolici}','${Id_Manupulo}','${Id_GerenteSo}',1,'${fechaHoraFormateada}','${Descripcion}','')`;
+    con.query(query,(error,result)=>{
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  })
+}
 
 //Roberto
 db.BuscarPacientes = (Paciente)=>{
