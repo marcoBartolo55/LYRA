53023c2059503b2a0e26720deb2348604b58d64a
3nr1qu30 <evarillasf2000@alumno.ipn.mx>
2023-05-14 20:12:03 -0600

    Casi todo lo de gonzalito

--- LYRA/src/app/public/js/Alertas.js
+++ LYRA/src/app/public/js/Alertas.js
@@ -17,6 +17,46 @@ function UsuarioOConInco(e){
   })
 };
 
+//Marco
+function SesionCaducada(e){
+  swal({
+      icon:'error',
+      title: '¡Error!',
+      text: "Tu sesión se ha caducado, por favor vuelve a iniciar sesión.",
+  }).then((confirmar)=>{
+    window.location.href='/InicioSesion';
+  });
+};
+
+//Marco
+function NoPermisos(e){
+  swal({
+      icon:'error',
+      title: '¡Error!',
+      text: "No tienes permiso para acceder a esta función, por favor vuelve a iniciar sesión.",
+  }).then((confirmar)=>{
+    window.location.href='/InicioSesion';
+  });
+};
+
+//Gonzalo
+function DatosNoActulizados(e){
+  swal({
+      icon:'error',
+      title: '¡Error!',
+      text: "Perfil no actualizado, porfavor intentelo más tarde",
+  });
+};
+
+//Gonzalo
+function PassNoActulizada(e){
+  swal({
+      icon:'error',
+      title: '¡Error!',
+      text: "Contraseña no actualizada, porfavor intentelo más tarde",
+  })
+};
+
 function ErrorServidor(e){
   swal({
       icon:'error',
@@ -36,3 +76,20 @@ function UsuarioRegistrado(e){
   });
 };
 
+//Gonzalo
+function DatosActulizados(e){
+  swal({
+      icon:'success',
+      title: '¡Éxito!',
+      text: "Perfil actualizado.",
+  });
+};
+
+//Gonzalo
+function PassActulizada(e){
+  swal({
+      icon:'success',
+      title: '¡Éxito!',
+      text: "Contraseña actualizada.",
+  });
+};
--- LYRA/src/app/routers/Psicologo.js
+++ LYRA/src/app/routers/Psicologo.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const PsicologoControllers = require('../controllers/PsicologoControllers');
+//get
+router.get('/', PsicologoControllers.Principal);
+//Gonzalo
+router.get('/EditarPerfil',PsicologoControllers.EditarDatos);
+//Post
+//Gonzalo
+router.post('/EditarPerfil', PsicologoControllers.EditarDatosPost);
+//Gonzalo
+router.post('/EditarPass', PsicologoControllers.EditarPass);
+
 module.exports= router;
